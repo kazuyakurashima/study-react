fbee80ae845784819dbf1b200010c71f50556819
kazuyakurashima <gs.kaz224@gmail.com>
2025-03-21 10:24:32 +0900

    🐛 軽微な修正

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from "../components/Footer";
 import { Links } from "@/components/Links";
 import { Headline } from "../components/Headline";
